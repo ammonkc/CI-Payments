d45f6a0434e2dcc52e348cab7ef689ed3350aedb
Dwayne Charrington <dwaynecharrington@gmail.com>
2011-02-16 10:51:14 +1000

    Added in process function for Paypal payments.

--- libraries/Payments/drivers/Payments_Paypal.php
+++ libraries/Payments/drivers/Payments_Paypal.php
@@ -41,8 +41,28 @@ class Payments_Paypal extends CI_Driver {
 
         $this->add_field('currency_code', $this->_api_config['currency_code']);
         $this->add_field('quantity', '1');
+        $this->add_field('no_note', '1');
+        $this->add_field('return', 'payment/success');
+        $this->add_field('cancel_return', 'payment/fail');
+        $this->add_field('notify_url', 'payment/notify');
 
         $this->_driver_fields['submit_button'] = "Please pay now";
+    }
+
+    public function process()
+    {
+        $str = '<h2>Preparing Transaction</h2>
+            <p style="text-align:center;"><img src="' . base_url() . 'img/slimbox/loading.gif" alt="loading" /></p>
+            <p style="text-align:center;">Please wait while your order is being processed.<br />You will be redirected to the paypal website.</p>
+            <p style="text-align:center;">If your browser does not redirect you, please<br />click the Continue button below to proceed.</p>
+            <form id="paypal" method="post" action="'.$this->_api_config['gateway_url'].'"><p style="text-align:center;padding-top:20px;">' . "\n";
+
+        foreach ($this->_fields as $name => $value)
+        {
+            $str .= '<input type="hidden" name="' . $name . '" value="' . $value . '" />' . "\n";
+        }
+        $str .= '<input type="submit" value="'.$this->_api_config["submit_button"].'" /></p></form>';
+        return $str;
     }
 
 }
